9456090dc5b7f30a3b15be707bcfcc6a7f14d0e0
OttawaSTEM <49529288+OttawaSTEM@users.noreply.github.com>
2021-08-24 19:22:39 +0000

    Deploying to gh-pages from @ OttawaSTEM/scratch-arduino@149fd89e7b39a2ac07fe220c2876aca65ba8b846 🚀

--- chunks/gui.js
+++ chunks/gui.js
@@ -156,13 +156,13 @@ var onClickLogo = function onClickLogo() {
 
 var onClickSignin = function onClickSignin() {
   {
-    True == 'True' ? window.open('http://127.0.0.1:8000/accounts/login/', '_self') : window.open('http://ottawastem.com/accounts/login/', '_self');
+     true ? window.open('http://127.0.0.1:8000/accounts/login/', '_self') : undefined;
   }
 };
 
 var onClickLogOut = function onClickLogOut() {
   {
-    True == 'True' ? window.open('http://127.0.0.1:8000/accounts/logout/', '_self') : window.open('http://ottawastem.com/accounts/logout/', '_self');
+     true ? window.open('http://127.0.0.1:8000/accounts/logout/', '_self') : undefined;
   }
 };
 
