7452e923f385a57407c1646dd7b651dde10ff114
one004u <one004u@naver.com>
2023-11-09 19:32:12 +0900

    feat: modify favicon to coconut

--- src/lib/default-project/index.js
+++ src/lib/default-project/index.js
@@ -6,6 +6,7 @@ import meowWav from '!arraybuffer-loader!./4e080acae1c3dc65eef30f07c2b0d4a5.wav'
 import backdrop from '!raw-loader!./cd21514d0531fdffb22204e0ec5ed84a.svg';
 import costume1 from '!raw-loader!./5de48e72637db4d7fc7f6bb526354114.svg';
 import costume2 from '!raw-loader!./48bbc82a10af3d0e0b7d5c677097df0e.svg';
+import costume3 from '!raw-loader!./coconut.svg';
 /* eslint-enable import/no-unresolved */
 
 const defaultProject = translator => {
@@ -50,6 +51,6 @@ const defaultProject = translator => {
         dataFormat: 'SVG',
         data: encoder.encode(costume2)
     }];
-};
+}; // TODO: coconut svg 추가
 
 export default defaultProject;
