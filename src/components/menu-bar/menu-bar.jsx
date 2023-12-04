9169338c5fb4f31875cf354f6ecad2ed4ec2ac71
one004u <one004u@naver.com>
2023-12-04 18:26:04 +0900

    feat: modify menubar

--- src/components/menu-bar/menu-bar.jsx
+++ src/components/menu-bar/menu-bar.jsx
@@ -806,8 +806,10 @@ class MenuBar extends React.Component {
                     <Divider className={classNames(styles.divider)} />
                 </div>
 
+                {/* show the proper UI in the account menu, given whether the user is
+                 logged in, and whether a session is available to log in with */}
                 <div className={styles.accountInfoGroup}>
-                    <SaveStatus />
+                    {/*<SaveStatus />*/}
                     <div className={styles.menuBarItem}>
                         {this.props.canSave && (
                             <SaveStatus />
